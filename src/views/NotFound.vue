bcf67c717e9e7dfd3f39aa54b57d7d11a5afdda5
vinhho3010 <anhvinh30102001@gmail.com>
2022-09-25 09:38:11 +0700

    Cai dat trang loi 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="page container">
+
+        <div class="row">
+            <div class="offset-md-4 col-md-6">
+                <p>
+                    Oops, Không thể tìm thấy trang. 
+                </p>
+            </div>
+        </div>
+
+        <div class="row">
+            <router-link class="offset-md-5 col-md-6" style="width:fit-content" to="/" >
+                <button class="btn btn-secondary btn-lg btn-block">Trở về trang chủ</button>
+            </router-link> 
+        </div>
+    </div>
+</template>
+
+<style scope>
+    .page{
+        margin-top: 30vh;
+    }
+    p{
+        padding-left: 25px;
+        font-size: 24px;
+    }
+</style>
